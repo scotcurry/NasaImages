5b2f81085e0c4b376fec87fb269fb4b327b12558
Scot Curry <scotcurry4@gmail.com>
2024-01-13 13:04:14 -0500

    Initial Commit

---
+++ NasaImages/NasaImagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NasaImagesApp.swift
+//  NasaImages
+//
+//  Created by Scot Curry on 1/13/24.
+//
+
+import SwiftUI
+
+@main
+struct NasaImagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
